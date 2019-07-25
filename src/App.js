f1518e432f94c5154f0722be994b1785554ab0bf
MichaelKozina <mike.kozina@gmail.com>
2019-07-25 08:48:15 -0400

    Setting up Tags

--- src/App.js
+++ src/App.js
@@ -12,13 +12,16 @@ class App extends Component {
     super();
     this.state = {
       showNote: false,
-      notes: []
+      notes: [],
+      note: {},
+      newTag: false
     };
   }
 
   toggleNote = () => {
     this.setState({
-      showNote: ! this.state.showNote
+      showNote: ! this.state.showNote,
+      note: {}
     });
   }
 
@@ -28,18 +31,68 @@ class App extends Component {
     .catch((err) => console.log(err.response.data) );
   }  
 
+  getNote = (id) => {
+    axios.get(urlFor(`notes/${id}`))
+    .then((res) => this.setState({ note: res.data, showNote: true }) )
+    .catch((err) => console.log(err.response.data) );
+  }
+
+  performSubmissionRequest = (data, id) => {
+    if (id) {
+      return axios.patch(urlFor('notes/${id}'), data);
+    } else {
+      return axios.post(urlFor('notes'), data);
+    }    
+  }
+
+  submitNote = (data, id) => {
+    this.performSubmissionRequest(data, id)  
+    .then((res) => this.setState({ showNote: false}) )
+    .catch((err) => console.log(err.response.data) );
+  }
+
+  deleteNote = (id) => {
+    const newNotesState = this.state.notes.filter((note) => note.id !== id);
+    axios.delete(urlFor(`notes/${id}`))
+    .then((res) => this.setState({ notes: newNotesState }))
+    .catch((err) => console.log(err.response.data) );
+  }
+
+  showTagForm = () => {
+    this.setState({ newTag: true});
+  }
+
+  closeTagForm = () => {
+    this.setState({ newTag: false });
+  }
+
+  submitTag = (data, noteId) => {
+    axios.post(urlFor(`notes/${noteId}/tags`), data)
+    .then((res) => this.getNote(noteId) )
+    .catch((err) => console.log(err.response.data) );
+  }
+
   render() {
-    const { showNote, notes } = this.state;
+    const { showNote, notes, note, newTag } = this.state;
  
     return (
       <div className="App">
         <Nav toggleNote={this.toggleNote} showNote={showNote} />  
         {showNote ?
-          <Note />
+          <Note 
+            note={note}
+            submitNote={this.submitNote}
+            showTagForm={this.showTagForm}
+            newTag={newTag}
+            closeTagForm={this.closeTagForm}
+            submitTag={this.submitTag}
+          />
           :
           <List
             getNotes={this.getNotes}
             notes={notes}
+            getNote={this.getNote}
+            deleteNote={this.deleteNote}
           />
         }
       </div>
